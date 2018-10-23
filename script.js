8216bf5110208f35969f1e01aec3cfa7607651ff
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-23 18:50:49 -0400

    Added div and h1 ussing DOM Manipulation. Linked stylesheet and scriptjs to indexhtml git status

---
+++ script.js
@@ -0,0 +1,4 @@
+let divOne = document.createElementbyID('Div1');
+let divOneClass = document.getElementsByClassName('header-container');
+let header = document.createElement('h1');
+let headerText = document.createTextNode('This is an h1');
