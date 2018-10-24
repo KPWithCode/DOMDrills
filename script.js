acef0de02192a90f4bf5c425c8b8ac0f79ab1284
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-24 13:49:58 -0400

    Coompleted 4th task on README

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let divOne = document.createElementbyID('Div1');
+
+let divOne = document.createElement('Div1');
 let divOneClass = document.getElementsByClassName('header-container');
 let header = document.createElement('h1');
 let headerText = document.createTextNode('This is an h1');
+
+header.appendChild(headerText);
+divOne.appendChild(header);
