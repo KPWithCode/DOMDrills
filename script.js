2793f4f3237c7c0eb3cf360b39de909b18cda376
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-24 19:02:13 -0400

    Move script tag to end of body

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ let divOneClass = document.getElementsByClassName('header-container');
 let header = document.createElement('h1');
 let headerText = document.createTextNode('This is an h1');
 
+
 header.appendChild(headerText);
 divOne.appendChild(header);
+
