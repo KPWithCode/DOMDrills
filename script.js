3e7532bb050fc559e7a1dea838fd65c3c8fda9ff
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-25 16:29:46 -0400

    Added event listener for each header

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     let headerContainer = document.createElement('div');
     let header = document.createElement('h1');
     let headerText = document.createTextNode('This is an h1');
@@ -31,4 +31,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let header6Text = document.createTextNode('This is an h6');
     h6.appendChild(header6Text);
     headerContainer.appendChild(h6);
+
+        // Event listener for each header
+    header.addEventListener('dblclick', colorChange);
+    h2.addEventListener('dblclick',colorChange);
+    h3.addEventListener('dblclick',colorChange);
+    h4.addEventListener('dblclick',colorChange);
+    h5.addEventListener('dblclick',colorChange);
+    h6.addEventListener('dblclick',colorChange);
+    function colorChange(event) {
+    let colorChange = ['Green', 'Pink', 'Yellow', 'Burgundy', 'black', 'brown'];
+    }
 });
